38dae789a49f23295261ae7cc976b6d0b3677e56
Khushi Chaudhary <khushichdry2912@gmail.com>
2024-11-07 12:24:05 +0530

    Navbar, HeroSection and Footer

--- safesisterslife/src/App.js
+++ safesisterslife/src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
+import Footer from './components/Footer';
+import HeroSection from './components/HeroSection';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navbar/>
+      <HeroSection/>
+      <Footer/>
+    </>
   );
 }
 
